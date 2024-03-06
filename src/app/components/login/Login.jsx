d22643a650653356c3ded9482cde99854483c0be
korneliafrankiewicz <nelcia6991@gmail.com>
2024-03-06 12:00:59 +0100

    colors palette updates

--- src/app/components/login/Login.jsx
+++ src/app/components/login/Login.jsx
@@ -1,11 +1,12 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Button from '@mui/material/Button';
 
-const loginIconStyles = {
+const loginIconStyles = (theme) => ({
   position: 'fixed',
   right: '20px',
   top: '20px',
-};
+  color: `${theme.palette.white}`,
+});
 
 const Login = () => {
   return (
