9a804833f8cef6cb8f1f63df895aafc6d3ab804f
Marek Mitis <marek.mitis@karhoo.com>
2024-04-29 22:25:26 +0200

    test fix

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    collectCoverage: true,
-    transform: {
-        '^.+\\.(ts|tsx)$': 'babel-jest',
-      },
-      setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-      moduleFileExtensions: ['js', 'jsx'],
-      testEnvironment: 'node'
-  };
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['js', 'jsx', 'tsx'],
+};
